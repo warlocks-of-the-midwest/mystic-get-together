2b1e964b44c9a3aa0927e8ee5ec2383ac4102500
git <akdflyinvee73@gmail.com>
2018-11-25 19:43:20 -0600

    Card hand view

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import GameArea from './components/GameArea';
+import Card from './components/Card';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<GameArea />, document.getElementById('root'));
+ReactDOM.render(<Card />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
