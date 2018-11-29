2deb9f858043d1448894ee33554f431a5327c8cb
akdombrowski <anthonydombrowski@pingidentity.com>
2018-11-29 11:25:58 -0600

    Added card render component.

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -9,32 +9,38 @@ class Card extends React.Component {
 	
 	render() {
 		return (
+			<div className="card-parent-container">
 			<div className="card-container">
-				<div className="name">
-					Sonic Assault	
-				</div>
-				<div className="cost">
-					{'{1}{U}{R}'}
+				<div className="name-mana-container">
+					<div className="name">
+						Sonic Assault	
+					</div>
+					<div className="cost">
+						{'{1}{U}{R}'}
+					</div>
 				</div>
 				<div className="image-container">
 					<img
 						className="card-image"
-						src={ require("./images/sonic-assault.jpg")}
+						src={ require("./images/aghoul.jpg")}
 					/>
 				</div>
-				<div className="type-container">
-					Instant
-				</div>
-				<div className="set-logo-container">
-					OO
+				<div className="type-set-container">
+					<div className="type-container">
+						Instant
+					</div>
+					<div className="set-logo-container">
+						OO
+					</div>
 				</div>
 				<div className="text-container">
-				Tap target creature. Sonic Assault deals 2 damage to that creature's controller.
-				Jump-start (You may cast this card from your graveyard by discarding a card in addition to paying its other costs. Then exile this card.)
+					Tap target creature. Sonic Assault deals 2 damage to that creature's controller.
+					Jump-start (You may cast this card from your graveyard by discarding a card in addition to paying its other costs. Then exile this card.)
 				</div>
 				<div className="power-toughness-container">
-					
+					3/2
 				</div>
+			</div>
 			</div>
 		);
 	}
