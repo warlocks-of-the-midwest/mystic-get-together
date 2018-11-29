e081de39625c862e987477afb99af3d5268bc4b1
akdombrowski <akdflyinvee73@gmail.com>
2018-11-29 11:45:32 -0600

    Client files for card component

---
+++ client/build/precache-manifest.cbdab9543138a5740037a8124e0c4fab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "07803ba8c2787d7f0f83",
+    "url": "/static/css/main.cd91f7dd.chunk.css"
+  },
+  {
+    "revision": "07803ba8c2787d7f0f83",
+    "url": "/static/js/main.07803ba8.chunk.js"
+  },
+  {
+    "revision": "20bc491bae5e667480bb",
+    "url": "/static/js/1.20bc491b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0dce222d76314b8e1d81c461004690c6",
+    "url": "/static/media/AbattoirGhoul.0dce222d.png"
+  },
+  {
+    "revision": "417e57c7a7af60e7a5d61dc7a50554f7",
+    "url": "/static/media/aghoul.417e57c7.jpg"
+  },
+  {
+    "revision": "15da7500181b0369667315430d387a02",
+    "url": "/index.html"
+  }
+];
