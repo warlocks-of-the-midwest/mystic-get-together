04bcafa3aeffa212da0d6552ba80997d8572c066
git <akdflyinvee73@gmail.com>
2018-12-02 08:14:57 -0600

    Resetting unused files to upstream master branch

--- client/src/components/GameArea.js
+++ client/src/components/GameArea.js
@@ -2,77 +2,22 @@ import React, { Component } from 'react';
 import '../styles/GameArea.css';
 
 class GameArea extends Component {
-  constructor(props) {
-    super(props);
-    
-    this.show_image = this.show_image.bind(this);
-  }
-  
-  show_image() {
-		var img = document.createElement("img");
-    img.src = "./Abattoir Ghoul.png";
-    img.width = 100;
-    img.height = 100;
-    img.alt = "Abbatoir";
-
-    return (<div>
-              <img src="AbatoirGhoul.png" heigh="100" width="100"/>
-            </div>);
-	}
-  
   render() {
     return (
       <div className="game-area">
-        
         <div className="board">
-        	<h1>Main Board</h1>
-          <div>
-            <img src= {require("./images/AbattoirGhoul.png")} height="300px" width="200px"/>
-          </div>
+        Main Board
         </div>
-        
         <div className="side-area">
-          
-          <div className="temp">
-          	<h4>Life</h4>
-          	<p>40</p>
-          </div>
-          
-          <div className="temp">
-          	<h4>Exile</h4>
-          	<img src="./images/AbattoirGhoul.png" height="150" width="150">
-          	</img>
-          </div>
-
-          <div className="temp">
-			<h4>Grave</h4>
-			<body>
-				<button onclick=
-    				"show_image('./AbattoirGhoul.png', 
-                 	276, 
-                 	110, 
-                 	'Abb');">"Show Graveyard"
-             	</button> 
-			</body>
-          </div>
-          
-          <div className="temp">
-			<h4>Hand</h4>
-          	<p>0</p>
-          </div>
-          
-          <div className="temp">
-			<h4>Library</h4>
-          	<p>40</p>
-          </div>
-        
+          <div className="temp">Life</div>
+          <div className="temp">Exile</div>
+          <div className="temp">Grave</div>
+          <div className="temp">Hand</div>
+          <div className="temp">Library</div>
         </div>
-      
       </div>
     );
   }
-
-  
 }
 
 export default GameArea;
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import GameArea from './components/GameArea';
-import Card from './components/Card';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Card />, document.getElementById('root'));
+ReactDOM.render(<GameArea />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
