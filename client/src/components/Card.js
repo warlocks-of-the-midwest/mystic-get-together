6483af357180e5ec6b508306995b51763db8a8fc
akdombrowski <akdflyinvee73@gmail.com>
2018-12-08 13:45:24 -0600

    Further changes on responsive sizing. Bold card name.

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -13,32 +13,31 @@ class Card extends React.Component {
 			<div className="card-container">
 				<div className="name-mana-container">
 					<div className="name">
-						Sonic Assault	
+						{this.props.name}
 					</div>
 					<div className="cost">
-						{'{1}{U}{R}'}
+						{this.props.cost}
 					</div>
 				</div>
 				<div className="image-container">
 					<img
 						className="card-image"
-						src={ require("./images/aghoul.jpg")}
+						src={this.props.image}
 					/>
 				</div>
 				<div className="type-set-container">
 					<div className="type-container">
-						Instant
+						{this.props.type}
 					</div>
 					<div className="set-logo-container">
-						OO
+						{this.props.set}
 					</div>
 				</div>
 				<div className="text-container">
-					Tap target creature. Sonic Assault deals 2 damage to that creature's controller.
-					Jump-start (You may cast this card from your graveyard by discarding a card in addition to paying its other costs. Then exile this card.)
+					{this.props.text}
 				</div>
 				<div className="power-toughness-container">
-					3/2
+					{this.props.power}/{this.props.toughness}
 				</div>
 			</div>
 			</div>
