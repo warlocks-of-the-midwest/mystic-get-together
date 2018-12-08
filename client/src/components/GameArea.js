78166486bbaa81206e950e06b17054d143b5968b
akdombrowski <akdflyinvee73@gmail.com>
2018-12-08 13:59:30 -0600

    Changes to show card on main GameArea

--- client/src/components/GameArea.js
+++ client/src/components/GameArea.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import '../styles/GameArea.css';
+import Card from './Card.js';
+import Hand from './Hand.js';
+import '../styles/Card.css';
+import '../styles/Hand.css';
 
 class GameArea extends Component {
   render() {
@@ -12,7 +16,12 @@ class GameArea extends Component {
           <div className="temp">Life</div>
           <div className="temp">Exile</div>
           <div className="temp">Grave</div>
-          <div className="temp">Hand</div>
+          <div className="temp">
+            <h1>Hand</h1>
+            <Card 
+
+            />
+          </div>
           <div className="temp">Library</div>
         </div>
       </div>
