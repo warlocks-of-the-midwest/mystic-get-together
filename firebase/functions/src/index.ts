57ad7304b62db5d2d2eaf705c6f7f77bf7a9eac0
Eric Zheng <eeeeeric@users.noreply.github.com>
2019-02-17 23:13:51 -0600

    Initialize user data when a user is first created
    Resolves #49
    
    When a user signs in for the first time, the user document under `Users/<uid>` is created and initialized with the `uid`, `username`, and `profile_picture` fields (although the picture URL may be undefined if the auth method does not provide it).
    
    When a user is deleted, the associated user document is deleted as well.
    
    Automated tests were added to cover these features.

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as Decklist from './decklist';
 import * as Game from './game';
+import * as User from './user';
 
 // Deck Functions
 export const importDeckFunction = Decklist.importDeckFunction;
@@ -10,3 +11,7 @@ export const populateDeckFunction = Decklist.populateDeckFunction;
 export const hostGameFunction = Game.hostGameFunction;
 export const joinGameFunction = Game.joinGameFunction;
 export const startGameFunction = Game.startGameFunction;
+
+// User Functions
+export const createUserDocumentFunction = User.createUserDocument;
+export const deleteUserDocumentFunction = User.deleteUserDocument;
---
+++ firebase/functions/test/test-utils.ts
@@ -0,0 +1,26 @@
+function sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+/**
+ * Attempts some operation up to 5 times, waiting 1 second between attempts.
+ * 
+ * @param operation The operation to attempt, which should return a Promise.
+ * If the Promise resolves, then that value is returned. If it rejects, then
+ * the operation is attempted again.
+ * @returns The return value of the operation
+ */
+export const attempt = async function(operation: () => Promise<any>): Promise<any> {
+    let attempts: number = 0;
+    let error: Error;
+    while (attempts < 5) {
+        try {
+            attempts++;
+            return await operation();
+        } catch (e) {
+            error = e;
+        }
+        await sleep(1000);
+    }
+    return Promise.reject(error);
+}
