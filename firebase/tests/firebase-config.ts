3c6da7430e5d6f094f68579a66d88b646c01016f
Eric Zheng <eeeeeric@users.noreply.github.com>
2019-03-09 21:08:25 -0600

    Added Security Rules for Firestore
    Added security rules for Firestore such that:
    - All reads require authentication (writes require authentiation and more)
    - Updates to a player's own data for a game can only be made by that player
    - Only players in a game can create additional cards in the game, and those cards must go to the Battlefield (the public zone). Note that currently, reads are allowed for the entire game, meaning a player does have read access to another player's cards in hand.
    - Only a card's controller may update it (e.g. tap it)

---
+++ firebase/tests/firebase-config.ts
@@ -0,0 +1,21 @@
+export const config = {
+    apiKey: "AIzaSyBZGipFIMdp-62lLu5rgOFv681I5w3Caeo",
+    authDomain: "mystic-the-get-together-test.firebaseapp.com",
+    databaseURL: "https://mystic-the-get-together-test.firebaseio.com",
+    projectId: "mystic-the-get-together-test",
+    storageBucket: "mystic-the-get-together-test.appspot.com",
+    messagingSenderId: "325028212595",
+};
+
+export const userInfo = {
+    user1: {
+        uid: 'JLI7eua7cnRYBYGdWbepVqZwQEg1',
+        email: 'function-tests-user-1@example.com',
+        password: 'password',
+    },
+    user2: {
+        uid: 'cKDlZPoerrVy3NbgR1a7HJmeW3E2',
+        email: 'function-tests-user-2@example.com',
+        password: 'password',
+    },
+};
